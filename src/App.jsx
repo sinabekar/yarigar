f3d57a31f1fe26d5d1430aa2f9cf16eab19ab995
RubiKamp <bekarsina@gmail.com>
2024-09-15 10:36:45 +0330

    i think its finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/haeder/Header";
 import Banner from "./components/banner/Banner";
 import Container from "./components/container/Conatiner";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <Header />
         <Banner />
         <Container />
+        <br /><br /><hr /><br /><b></b>
+        <Footer />
       </main>
     </>
   );
--- src/components/haeder/Header.jsx
+++ src/components/haeder/Header.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Logo from '../../assets/logo.png'
+import React from "react";
+import Logo from "../../assets/logo.png";
 
 export default function Header() {
   return (
-    <div className='w-full h-[100px] flex justify-between items-center'>
-      <div className='flex'>
-        <img src={Logo} alt="logo" width={50} height={50}/>
+    <div className="w-full h-[100px] flex justify-between items-center">
+      <div className="flex">
+        <img src={Logo} alt="logo" width={50} height={50} />
         {/* <a href="#">
           <h3 className='text-2xl'>پرستاران</h3>
         </a> */}
       </div>
-      <button className='border-4 border-[#00bf63] h-10 rounded-lg px-4'><a href="#">انتخاب همیار</a></button>
+      <button className="border-4 border-[#00bf63] h-10 rounded-lg px-4">
+        <a href="https://survey.porsline.ir/s/X72a8q64">انتخاب همیار</a>
+      </button>
     </div>
-  )
+  );
 }
