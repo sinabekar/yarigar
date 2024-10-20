cd030be33a918b20fc2458225717361b69c00f80
RubiKamp <bekarsina@gmail.com>
2024-10-20 11:22:34 +0330

    updata

--- src/components/banner/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Banner() {
-  return (
-    <a className="w-full h-full" href="https://survey.porsline.ir/s/X72a8q64">
-      <div className="w-full h-60 rounded-[20px] bg-green-500"></div>
-    </a>
-  );
-}
